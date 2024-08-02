7677744b1bdea09a0961e0b31aef46c8b0064462
Edison82 <ediflow82@gmail.com>
2024-08-01 23:20:41 -0500

    Config basic Server

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import {dirname, join} from 'path' // Se usa este modulo de node para crear la ruta absoluta y join concatena directoriosin importar sistema operativo
+import { fileURLToPath } from 'url' // Se usa para 
+
+const app = express() 
+const __dirname = dirname(fileURLToPath(import.meta.url)) //Crea ruta absoluta c://destop/
+
+
+app.set('views', join(__dirname, 'views'))
+app.set('view engine', 'ejs')  //Activa motor de plantillas
+
+app.get('/', (req,res)=> res.render('index.ejs')) // Da respuesta al llamar a la pagina principal
+
+app.listen(3000)
+console.log('Server is listening on port', 3000)
